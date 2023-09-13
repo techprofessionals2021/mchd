c1beb71cddc035f55b5c0c99f37226684a1ed2cb
syedhammadali1 <hammadali407292@gmail.com>
2023-09-13 15:45:29 +0500

    .

--- storage/framework/views/afaa8d24486b7353e8bbd7b59bef65b368ab1dc1.php
+++
@@ -1,6 +0,0 @@
-<li class="dash-item dash-hasmenu<?php echo e((Request::route()->getName() == 'landingpage.index') ? ' active' : ''); ?>">
-    <a href="<?php echo e(route('landingpage.index')); ?>" class="dash-link">
-        <span class="dash-micon"><i class="ti ti-settings"></i></span><span class="dash-mtext"><?php echo e(__('Landing Page')); ?></span>
-    </a>
-</li>
-<?php /**PATH D:\laragon\www\mchd\v1\main-file\Modules/LandingPage\Resources/views/menu/landingpage.blade.php ENDPATH**/ ?>
