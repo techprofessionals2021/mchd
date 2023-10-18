3daabe0fc34996ea9ea85f7706d9799389d534b8
syedhammadali1 <hammadali407292@gmail.com>
2023-10-18 20:03:51 +0500

    make calender colour ful

--- resources/js/Components/Custom/Qalendar.vue
+++ resources/js/Components/Custom/Qalendar.vue
@@ -1,5 +1,5 @@
 <template>
-    <Qalendar :events="events" :config="config">
+    <Qalendar  :events="events" :config="config" @date-was-clicked="handleDateClicked">
         <template #eventDialog="props">
             <div v-if="props.eventDialogData && props.eventDialogData.title">
                 <h3 :style="{ marginBottom: '8px' }" class="text-center mt-2">Meeting Details</h3>
@@ -52,17 +52,52 @@
         </button> -->
             </div>
         </template>
+
+        <template v-slot:event="{ event }">
+            <div class="custom-event" :style="'background-color: ' + event.color">
+                {{ event.title }}
+            </div>
+        </template>
     </Qalendar>
+
+    <!-- <div> -->
+        <!-- <a-button type="primary" @click="showModal">Open Modal</a-button>
+        <a-button @click="success">Success</a-button> -->
+        <!-- <a-modal v-model:open="open" title="Basic Modal">
+            </a-modal> -->
+    <!-- </div> -->
 </template>
 
 <script>
 import { Qalendar } from "qalendar";
 import { Avatar } from "./Avatar";
-
+import { meeting } from "./MeetingFormModal.vue";
+import { Modal } from 'ant-design-vue';
+import { ref } from 'vue';
+const open = ref(false);
 export default {
     components: {
         Qalendar,
-        Avatar
+        Avatar,
+        meeting
+    },
+    methods: {
+        // handleDateClicked(value) {
+        //     alert(value);
+        //     open.value = true;
+        // },
+        showModal() {
+            open.value = true;
+        },
+        handleOk(e) {
+            console.log(e);
+            open.value = false;
+        },
+        success() {
+            Modal.success({
+                title: 'This is a success message',
+            });
+        }
     },
 
     data() {
@@ -70,25 +105,60 @@ export default {
             events: [
                 // ...
                 {
-                    title: "Advanced algebra",
-                    with: "Chandler Bing",
-                    time: { start: "2023-10-17 12:05", end: "2023-10-20 13:35" },
-                    color: "yellow",
-                    isEditable: true,
+                    title: "Meeting For Health",
+                    // with: "Chandler Bing",
+                    time: { start: "2023-10-18 07:05", end: "2023-10-18 09:00" },
+                    color: "green",
+                    // isEditable: true,
                     id: "753944708f0f",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
                 },
                 {
-                    title: "Ralph on holiday",
-                    with: "Rachel Greene",
-                    time: { start: "2022-05-10", end: "2022-05-22" },
-                    color: "green",
-                    isEditable: true,
-                    id: "5602b6f589fc"
-                }
-                // ...
+                    title: "Meeting For Work",
+                    // with: "Chandler Bing",
+                    time: { start: "2023-10-21 08:05", end: "2023-10-21 11:00" },
+                    color: "red",
+                    // isEditable: true,
+                    id: "7539448f0f",
+                    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                },
+                {
+                    title: "Meeting For Food",
+                    // with: "Chandler Bing",
+                    time: { start: "2023-10-20 11:05", end: "2023-10-20 13:00" },
+                    color: "yellow",
+                    // isEditable: true,
+                    id: "753944f0f",
+                    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                },
+                {
+                    title: "Meeting For Sport",
+                    // with: "Chandler Bing",
+                    time: { start: "2023-10-21 11:05", end: "2023-10-21 13:00" },
+                    color: "brown",
+                    // isEditable: true,
+                    id: "7539f0f",
+                    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                },
+                {
+                    title: "Meeting For Food",
+                    // with: "Chandler Bing",
+                    time: { start: "2023-10-16 11:05", end: "2023-10-16 13:00" },
+                    color: "blue",
+                    // isEditable: true,
+                    id: "753944f0f",
+                    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                },
             ],
             config: {
+                height:'2000px',
+                eventStyle: {
+                    background: this.eventBackgroundColor, // Bind background color
+                },
+                dayBoundaries: {
+                    start: 6,
+                    end: 18,
+                },
                 // see configuration section
                 eventDialog: {
                     isCustom: true
@@ -96,23 +166,44 @@ export default {
                 style: {
                     // When adding a custom font, please also set the fallback(s) yourself
                     fontFamily: '',
-
                 },
+            },
+            visible: false,
+            form: {
+                title: '',
+                description: '',
+                time: null,
+                assignee: '',
+            },
+            open,
+            eventBackgroundColor: 'yellow',
         }
-    }
-},
+    },
 }
 </script>
 
 <style>
 /* @import "qalendar/dist/style.css"; */
 
-.week-timeline__event{
+.week-timeline__event {
     height: 2rem !important;
 }
 
-.calendar-month__event{
-    background: bisque !important;
+.calendar-month__event {
+    /* background: bisque !important; */
     height: 2rem !important;
+}
+
+.custom-event {
+    color: white;
+    /* Text color for the event */
+    padding: 4px;
+    border-radius: 4px;
+    text-align: center;
+    cursor: pointer;
+}
+
+.mode-is-month{
+    height: 40rem !important;
 }
 </style>
