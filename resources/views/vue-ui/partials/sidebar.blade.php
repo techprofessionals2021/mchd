83734eefc93731828a8da00aa9446f4eba37dc33
syedhammadali1 <hammadali407292@gmail.com>
2023-11-14 21:52:20 +0500

    .

--- resources/views/vue-ui/partials/sidebar.blade.php
+++ resources/views/vue-ui/partials/sidebar.blade.php
@@ -289,13 +289,13 @@ style="border-right: 1px solid ">
                                     src="{{ asset('custom-ui/images/calendar.png') }}" class="icon-image" /></span><span
                                 class="dash-mtext side-nav-text">{{ __('Calendar') }}</span></a>
                     </li>
-                     <li class="dash-item dash-hasmenu">
+                     {{-- <li class="dash-item dash-hasmenu">
                         <a href="{{ route('custom.huddles',[$currentWorkspace->slug]) }}"
                             class="dash-link{{ Request::route()->getName() == 'custom.huddles' ? ' active' : '' }} side-item"><span
                                 class="dash-micon mr-3"> <img
                                     src="{{ asset('custom-ui/images/calendar.png') }}" class="icon-image" /></span><span
                                 class="dash-mtext side-nav-text">{{ __('Committees') }}</span></a>
-                    </li>
+                    </li> --}}
 
                 @if (auth()->user()->hasRole(('HOD')) )
                     <li class="dash-item dash-hasmenu">
