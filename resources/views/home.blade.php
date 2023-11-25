189bdce2c12a05006597152d6f892eea95a6b249
syedhammadali1 <hammadali407292@gmail.com>
2023-11-25 22:46:45 +0500

    ceo and single executive report

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -384,7 +384,7 @@
                      </div>
 
 
-                    @if (auth()->user()->hasRole('Ceo') && $check_home == 1)
+                    @if (auth()->user()->hasRole('CEO') && $check_home == 1)
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-header">
